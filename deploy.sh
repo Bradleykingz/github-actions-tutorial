66317ae33df46e3c1fcbe78dacfb92606231dd9b
Bradley Kingsley <bradleykigsley2@gmail.com>
2020-06-28 01:13:28 +0300

    chore: revert

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-mkdir -p ~/my-express-app/
-cd ~/my-express-app/ || exit
-git clone https://github.com/Bradleykingz/github-actions-tutorial app
-cd app || exit
-npm install
-pm2 start app.js
